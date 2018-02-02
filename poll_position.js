5b68b897c6072e182d9561367e7a5fbbb78fb130
Kevin Vennitti <kevin.vennitti@gmail.com>
2018-02-02 11:56:36 +0100

    Changes

--- poll_position.js
+++ poll_position.js
@@ -3,6 +3,8 @@ var fs = require('fs')
 var five = require("johnny-five");
 // var board = new five.Board();
 
+var twitterUsernameIdRecevingPollResults = '928204573642231808'; // http://gettwitterid.com/?user_name=Bastientestdsaa
+
 var T = new Twit({
   consumer_key:         'fR0WktkLF4G3rm9OfdbMOsQny',
   consumer_secret:      'zq7AgRVRySGnKSM5HPSGoJ1T9f2po0vqvvlm9hWkFSOmPCUZrt',
@@ -27,21 +29,21 @@ var SAONE = 2;
 var polls = [
   {
     team1 : {
-      hashtag: '#ixda_1',
+      hashtag: 'ixda_1',
       score: 0
     },
     team2 : {
-      hashtag: '#ixda_2',
+      hashtag: 'ixda_2',
       score: 0
     }
   },
   {
     team1 : {
-      hashtag: '#ixda_A',
+      hashtag: 'ixda_A',
       score: 0
     },
     team2 : {
-      hashtag: '#ixda_B',
+      hashtag: 'ixda_B',
       score: 0
     }
   }
@@ -60,130 +62,156 @@ function doPoll() {
 
   stream.on('tweet', function(tweet) {
     processTweet(tweet);
-  }).on('error', function() {
-    console.log('error');
   });
 }
 
 doPoll();
 
-
 function processTweet( tweet ) {
-  //console.log(tweet)
-  //console.log(tweet.id_str)
 
-console.log('new tweet!');
-
+  var team;
   var tweet_hashtags = tweet.entities.hashtags;
-  for (var i = 0; i <= tweet_hashtags.length; i++) {
-    console.log(tweet_hashtags[i].text);
+
+  for (var i = 0; i < tweet_hashtags.length; i++) {
+    if (tweet_hashtags[i].text == polls[currentPoll].team1.hashtag) {
+      team = 'team1';
+    }
+    if (tweet_hashtags[i].text == polls[currentPoll].team2.hashtag) {
+      team = 'team2';
+    }
   }
-  /*
-  var team = team;
+
   var id = tweet.id;
 
   polls[currentPoll][team].score++;
 
+  if (team == 'team1') { //put the post media here (red) /!\ modulo put this in an other if
+    var led = new five.Led(13);
+    led.on();
+    setTimeout(function() {
+      led.off();
+    }, (300));
+  }
+  else { //blue here
+    var led = new five.Led(12);
+    led.on();
+
+    setTimeout(function() {
+      led.off();
+    }, (300));
+  }
+
   console.log('Score Team 1 : '+polls[currentPoll].team1.score);
   console.log('Score Team 2 : '+polls[currentPoll].team2.score);
-  */
 
-  //  if (scoreboard[tweet.user.screen_name].score % 3 == 0) {
-  //    console.log("score 1");
-  //    var b64contentred = fs.readFile('D:\\Documents\\GitHub\\Twitometer\\gif\\lap_red.gif', { encoding: 'base64' })
+  sendPollResults();
 
-  //    // first we must post the media to Twitter
-  //    T.post('media/upload', { media_data: b64contentred }, function (err, data, response) {
-  //      // now we can assign alt text to the media, for use by screen readers and
-  //      // other text-based presentations and interpreters
-  //      var mediaIdStr = data.media_id_string
-  //      var altText = "You completed an entire lap, congrats !"
-  //      var meta_params = { media_id: mediaIdStr, alt_text: { text: altText } }
+/*
+   if (scoreboard[tweet.user.screen_name].score % 3 == 0) {
+     console.log("score 1");
+     var b64contentred = fs.readFile('D:\\Documents\\GitHub\\Twitometer\\gif\\lap_red.gif', { encoding: 'base64' })
 
-  //      T.post('media/metadata/create', meta_params, function (err, data, response) {
-  //        if (!err) {
-  // now we can reference the media and post a tweet (media will attach to the tweet)
-  //          var params = { status: '@'+tweet.user.screen_name+' You completed a lap !', media_ids: [mediaIdStr] }
-  //
-  //          T.post('statuses/update', params, function (err, data, response) {
-  //            console.log(data)
-  //          })
-  //        }
-  //      })
-  //  })
-  //  }
+     // first we must post the media to Twitter
+     T.post('media/upload', { media_data: b64contentred }, function (err, data, response) {
+       // now we can assign alt text to the media, for use by screen readers and
+       // other text-based presentations and interpreters
+       var mediaIdStr = data.media_id_string
+       var altText = "You completed an entire lap, congrats !"
+       var meta_params = { media_id: mediaIdStr, alt_text: { text: altText } }
 
+       T.post('media/metadata/create', meta_params, function (err, data, response) {
+         if (!err) {
+  now we can reference the media and post a tweet (media will attach to the tweet)
+           var params = { status: '@'+tweet.user.screen_name+' You completed a lap !', media_ids: [mediaIdStr] }
 
-  /*
-  if (team === 1) { //put the post media here (red) /!\ modulo put this in an other if
-  var led = new five.Led(13);
-  led.on();
-  setTimeout(function() {
-  led.off();
-}, (300));
-}
-else { //blue here
-var led = new five.Led(12);
-led.on();
-setTimeout(function() {
-led.off();
-}, (300));
-}
+           T.post('statuses/update', params, function (err, data, response) {
+             console.log(data)
+           })
+         }
+       })
+   })
+   }
 */
 
 
 
-// setInterval(function(){check_time();},30000); //check every 10min
-// function check_time() {
-//   if (Date.now() > 151067340000) {
-//     //console.log('oui')
-//     var result = [];
-//     for(var username in scoreboard){
-//       var user = {};
-//       //scoreboard[username]
-//       user.name = username;
-//       user.team = scoreboard[username].team;
-//       user.score = scoreboard[username].score;
-//       result.push(user);
-//     }
-//     //console.log(result)
-//     result.sort(
-//       function(a,b) {
-//         return a.score - b.score;
-//       }
-//     );
-//     result.reverse();
-//
-//     //for (var i = 0; i < 3; i++) {
-//     //result[i]
-//
-//     var b64contentend = fs.readFile('./gif/fin.gif', { encoding: 'base64' }, function(err,data){
-//       // console.log(data);
-//
-//           // first we must post the media to Twitter
-//           T.post('media/upload', { media_data: b64contentend }, function (err, data, response) {
-//             // now we can assign alt text to the media, for use by screen readers and
-//             // other text-based presentations and interpreters
-//             var mediaIdStr = data.media_id_string
-//             var altText = "You completed an entire lap, congrats !"
-//             var meta_params = { media_id: mediaIdStr, alt_text: { text: altText } }
-//
-//             T.post('media/metadata/create', meta_params, function (err, data, response) {
-//               if (!err) {
-//                 // now we can reference the media and post a tweet (media will attach to the tweet)
-//                 var params = { status: '@'+result[0].name+'@'+result[1].name+'@'+result[2].name, media_ids: [mediaIdStr] }
-//
-//                 T.post('statuses/update', params, function (err, data, response) {
-//                   console.log(data)
-//                 })
-//               }
-//             })
-//           })
-//
-//     })
-//
-//
-//
-//       //console.log(result)
-//
+/*
+setInterval(function(){check_time();},30000); //check every 10min
+function check_time() {
+  if (Date.now() > 151067340000) {
+    //console.log('oui')
+    var result = [];
+    for(var username in scoreboard){
+      var user = {};
+      //scoreboard[username]
+      user.name = username;
+      user.team = scoreboard[username].team;
+      user.score = scoreboard[username].score;
+      result.push(user);
+    }
+    //console.log(result)
+    result.sort(
+      function(a,b) {
+        return a.score - b.score;
+      }
+    );
+    result.reverse();
+
+    //for (var i = 0; i < 3; i++) {
+    //result[i]
+
+    var b64contentend = fs.readFile('./gif/fin.gif', { encoding: 'base64' }, function(err,data){
+      // console.log(data);
+
+          // first we must post the media to Twitter
+          T.post('media/upload', { media_data: b64contentend }, function (err, data, response) {
+            // now we can assign alt text to the media, for use by screen readers and
+            // other text-based presentations and interpreters
+            var mediaIdStr = data.media_id_string
+            var altText = "You completed an entire lap, congrats !"
+            var meta_params = { media_id: mediaIdStr, alt_text: { text: altText } }
+
+            T.post('media/metadata/create', meta_params, function (err, data, response) {
+              if (!err) {
+                // now we can reference the media and post a tweet (media will attach to the tweet)
+                var params = { status: '@'+result[0].name+'@'+result[1].name+'@'+result[2].name, media_ids: [mediaIdStr] }
+
+                T.post('statuses/update', params, function (err, data, response) {
+                  console.log(data)
+                })
+              }
+            })
+          })
+
+    })
+
+
+
+      //console.log(result)
+*/
 }
+
+function lookForNewPollOrder() {
+  T.get('direct_messages/events/list', {}, function(err, data, response) {
+    console.log(data);
+  });
+}
+
+lookForNewPollOrder();
+
+function sendPollResults() {
+  T.post('direct_messages/events/new', { event: { type: 'message_create', message_create: {
+      "target": {
+        "recipient_id": twitterUsernameIdRecevingPollResults
+      },
+      "message_data": {
+        "text": 'Hello ! La team #'+polls[currentPoll].team1.hashtag+' a eu '+polls[currentPoll].team1.score+' votes et la team #'+polls[currentPoll].team2.hashtag+' a eu '+polls[currentPoll].team2.score+' votes.',
+      }
+    }
+  }
+} , function(err, data, response) {
+    console.log(data)
+  })
+}
+
+sendPollResults();
