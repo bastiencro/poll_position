2440f2eed6d125f5deeaa4772c4cfa9b3f17646a
Kevin Vennitti <kevin.vennitti@gmail.com>
2018-02-03 10:03:10 +0100

    UI

--- poll_position.js
+++ poll_position.js
@@ -8,7 +8,8 @@ var twitterUsernameIdRecevingPollResults = '928204573642231808'; // http://gettw
 var Twit = require('twit')
 var fs = require('fs')
 var five = require("johnny-five");
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
@@ -23,19 +24,6 @@ var T = new Twit({
 if (useArduino == true) {
   var board = new five.Board();
 }
-
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
-
-io.on('connection', function(socket){
-  console.log('a user connected');
-});
-
-http.listen(3000, function(){
-  console.log('listening on *:3000');
-});
-
 
 var polls = [
   {
@@ -63,66 +51,165 @@ var polls = [
 var stream;
 var currentPoll = 0;
 
-function doPoll() {
-  console.log('-----------');
-  console.log('New Poll (id: '+currentPoll+')');
-  console.log('> Team 1 hashtag: '+polls[currentPoll].team1.hashtag);
-  console.log('> Team 2 hashtag: '+polls[currentPoll].team2.hashtag);
-  console.log('Let\'s go!');
-
-  stream = T.stream('statuses/filter', { track: [polls[currentPoll].team1.hashtag, polls[currentPoll].team2.hashtag] })
-
-  stream.on('tweet', function(tweet) {
-    processTweet(tweet);
-  });
-}
-
 var tweet_team1 = {"created_at":"Fri Feb 02 12:58:05 +0000 2018","id":959410594226098200,"id_str":"959410594226098177","text":"#ixda_1 ghj","source":"<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>","truncated":false,"in_reply_to_status_id":null,"in_reply_to_status_id_str":null,"in_reply_to_user_id":null,"in_reply_to_user_id_str":null,"in_reply_to_screen_name":null,"user":{"id":928204573642231800,"id_str":"928204573642231808","name":"Bastien_test","screen_name":"Bastientestdsaa","location":null,"url":null,"description":null,"translator_type":"none","protected":false,"verified":false,"followers_count":0,"friends_count":2,"listed_count":0,"favourites_count":1,"statuses_count":155,"created_at":"Wed Nov 08 10:16:30 +0000 2017","utc_offset":null,"time_zone":null,"geo_enabled":false,"lang":"fr","contributors_enabled":false,"is_translator":false,"profile_background_color":"F5F8FA","profile_background_image_url":"","profile_background_image_url_https":"","profile_background_tile":false,"profile_link_color":"1DA1F2","profile_sidebar_border_color":"C0DEED","profile_sidebar_fill_color":"DDEEF6","profile_text_color":"333333","profile_use_background_image":true,"profile_image_url":"http://pbs.twimg.com/profile_images/928902467882029057/cf-Oz-Oz_normal.jpg","profile_image_url_https":"https://pbs.twimg.com/profile_images/928902467882029057/cf-Oz-Oz_normal.jpg","default_profile":true,"default_profile_image":false,"following":null,"follow_request_sent":null,"notifications":null},"geo":null,"coordinates":null,"place":null,"contributors":null,"is_quote_status":false,"quote_count":0,"reply_count":0,"retweet_count":0,"favorite_count":0,"entities":{"hashtags":[{"text":"ixda_1","indices":[0,7]}],"urls":[],"user_mentions":[],"symbols":[]},"favorited":false,"retweeted":false,"filter_level":"low","lang":"und","timestamp_ms":"1517576285307"};
 
 var tweet_team2 = {"created_at":"Fri Feb 02 12:58:05 +0000 2018","id":959410594226098200,"id_str":"959410594226098177","text":"#ixda_2 ghj","source":"<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>","truncated":false,"in_reply_to_status_id":null,"in_reply_to_status_id_str":null,"in_reply_to_user_id":null,"in_reply_to_user_id_str":null,"in_reply_to_screen_name":null,"user":{"id":928204573642231800,"id_str":"928204573642231808","name":"Bastien_test","screen_name":"Bastientestdsaa","location":null,"url":null,"description":null,"translator_type":"none","protected":false,"verified":false,"followers_count":0,"friends_count":2,"listed_count":0,"favourites_count":1,"statuses_count":155,"created_at":"Wed Nov 08 10:16:30 +0000 2017","utc_offset":null,"time_zone":null,"geo_enabled":false,"lang":"fr","contributors_enabled":false,"is_translator":false,"profile_background_color":"F5F8FA","profile_background_image_url":"","profile_background_image_url_https":"","profile_background_tile":false,"profile_link_color":"1DA1F2","profile_sidebar_border_color":"C0DEED","profile_sidebar_fill_color":"DDEEF6","profile_text_color":"333333","profile_use_background_image":true,"profile_image_url":"http://pbs.twimg.com/profile_images/928902467882029057/cf-Oz-Oz_normal.jpg","profile_image_url_https":"https://pbs.twimg.com/profile_images/928902467882029057/cf-Oz-Oz_normal.jpg","default_profile":true,"default_profile_image":false,"following":null,"follow_request_sent":null,"notifications":null},"geo":null,"coordinates":null,"place":null,"contributors":null,"is_quote_status":false,"quote_count":0,"reply_count":0,"retweet_count":0,"favorite_count":0,"entities":{"hashtags":[{"text":"ixda_2","indices":[0,7]}],"urls":[],"user_mentions":[],"symbols":[]},"favorited":false,"retweeted":false,"filter_level":"low","lang":"und","timestamp_ms":"1517576285307"};
 
+app.use(express.static('www'));
 
-function processTweet( tweet ) {
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/www/index.html');
+});
 
-  console.log(JSON.stringify(tweet));
+io.on('connection', function(socket){
+  console.log('a user connected');
 
-  var team;
-  var tweet_hashtags = tweet.entities.hashtags;
+  function doPoll() {
+    console.log('-----------');
+    console.log('New Poll (id: '+currentPoll+')');
+    console.log('> Team 1 hashtag: '+polls[currentPoll].team1.hashtag);
+    console.log('> Team 2 hashtag: '+polls[currentPoll].team2.hashtag);
+    console.log('Let\'s go!');
 
-  for (var i = 0; i < tweet_hashtags.length; i++) {
-    if (tweet_hashtags[i].text == polls[currentPoll].team1.hashtag) {
-      team = 'team1';
+    stream = T.stream('statuses/filter', { track: [polls[currentPoll].team1.hashtag, polls[currentPoll].team2.hashtag] })
+
+    stream.on('tweet', function(tweet) {
+      processTweet(tweet);
+    });
+  }
+
+
+  function processTweet( tweet ) {
+
+    var team = null;
+    var tweet_hashtags = tweet.entities.hashtags;
+
+    for (var i = 0; i < tweet_hashtags.length; i++) {
+      if (tweet_hashtags[i].text == polls[currentPoll].team1.hashtag) {
+        team = 'team1';
+      }
+      if (tweet_hashtags[i].text == polls[currentPoll].team2.hashtag) {
+        team = 'team2';
+      }
     }
-    if (tweet_hashtags[i].text == polls[currentPoll].team2.hashtag) {
-      team = 'team2';
+
+    if (team == null) {
+      return;
     }
+
+    var id = tweet.id;
+
+    polls[currentPoll][team].score++;
+
+    if (useArduino == true) {
+      if (team == 'team1') { //put the post media here (red) /!\ modulo put this in an other if
+        var led = new five.Led(13);
+        led.on();
+        setTimeout(function() {
+          led.off();
+        }, (3000));
+      }
+      else { //blue here
+        var led = new five.Led(12);
+        led.on();
+
+        setTimeout(function() {
+          led.off();
+        }, (3000));
+      }
+    }
+
+
+    console.log('Score Team 1 : '+polls[currentPoll].team1.score);
+    console.log('Score Team 2 : '+polls[currentPoll].team2.score);
+
+    socket.emit('scores/update', {
+      last_tweet: {
+        text: tweet.text
+      },
+      poll: polls[currentPoll],
+    });
+
+    sendPollResults();
   }
 
-  var id = tweet.id;
 
-  polls[currentPoll][team].score++;
+  function lookForNewPollOrder() {
+    T.get('direct_messages/events/list', {}, function(err, data, response) {
+      console.log(data);
+      if (data.events) {
+      for (var j = 0; j < data.events.length; j++) {
+        if (data.events[j].message_create.target.recipient_id == '915913284779376640') {
 
-  if (team == 'team1') { //put the post media here (red) /!\ modulo put this in an other if
-    var led = new five.Led(13);
-    led.on();
-    setTimeout(function() {
-      led.off();
-    }, (3000));
+          for (var k = 0; k < data.events[j].message_create.message_data.entities.hashtags.length; k++) {
+            var hashtag = data.events[j].message_create.message_data.entities.hashtags[k].text;
+
+            if (hashtag.substr(0,5) == 'POLL_') {
+              var idNewPoll = parseInt(hashtag.substr(5));
+              console.log(idNewPoll);
+
+              if (idNewPoll != currentPoll) {
+                currentPoll = Math.min(Math.max(0, idNewPoll), polls.length-1);
+                doPoll();
+                return;
+              }
+            }
+
+          }
+
+        }
+      }
+      }
+    });
   }
-  else { //blue here
-    var led = new five.Led(12);
-    led.on();
 
-    setTimeout(function() {
-      led.off();
-    }, (3000));
+  function sendPollResults() {
+    /*
+    T.post('direct_messages/events/new', { event: { type: 'message_create', message_create: {
+        "target": {
+          "recipient_id": twitterUsernameIdRecevingPollResults
+        },
+        "message_data": {
+          "text": 'Hello ! La team #'+polls[currentPoll].team1.hashtag+' a eu '+polls[currentPoll].team1.score+' votes et la team #'+polls[currentPoll].team2.hashtag+' a eu '+polls[currentPoll].team2.score+' votes.',
+        }
+      }
+    }
+  } , function(err, data, response) {
+      console.log(data)
+    })*/
   }
 
+  if (useArduino == true) {
+    board.on("ready", function() {
+      startScript();
+    });
+  } else {
+    startScript();
+  }
 
-  console.log('Score Team 1 : '+polls[currentPoll].team1.score);
-  console.log('Score Team 2 : '+polls[currentPoll].team2.score);
+  function startScript() {
+    doPoll();
+    // lookForNewPollOrder();
+    // sendPollResults();
 
-  sendPollResults();
+    //processTweet(tweet_team1);
+    // processTweet(tweet_team2);
+
+    setInterval(function(){
+      if (Math.random() < .5) {
+        processTweet(tweet_team1);
+      } else {
+        processTweet(tweet_team2);
+      }
+    },3000);
+  }
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+
+
 
 /*
    if (scoreboard[tweet.user.screen_name].score % 3 == 0) {
@@ -207,62 +294,3 @@ function check_time() {
 
       //console.log(result)
 */
-}
-
-function lookForNewPollOrder() {
-  T.get('direct_messages/events/list', {}, function(err, data, response) {
-    console.log(data);
-    for (var j = 0; j < data.events.length; j++) {
-      if (data.events[j].message_create.target.recipient_id == '915913284779376640') {
-
-        for (var k = 0; k < data.events[j].message_create.message_data.entities.hashtags.length; k++) {
-          var hashtag = data.events[j].message_create.message_data.entities.hashtags[k].text;
-
-          if (hashtag.substr(0,5) == 'POLL_') {
-            var idNewPoll = parseInt(hashtag.substr(5));
-            console.log(idNewPoll);
-
-            if (idNewPoll != currentPoll) {
-              currentPoll = parseInt*(idNewPoll);
-              doPoll();
-              return;
-            }
-          }
-
-        }
-
-      }
-    }
-  });
-}
-
-function sendPollResults() {
-  /*
-  T.post('direct_messages/events/new', { event: { type: 'message_create', message_create: {
-      "target": {
-        "recipient_id": twitterUsernameIdRecevingPollResults
-      },
-      "message_data": {
-        "text": 'Hello ! La team #'+polls[currentPoll].team1.hashtag+' a eu '+polls[currentPoll].team1.score+' votes et la team #'+polls[currentPoll].team2.hashtag+' a eu '+polls[currentPoll].team2.score+' votes.',
-      }
-    }
-  }
-} , function(err, data, response) {
-    console.log(data)
-  })*/
-}
-
-
-
-board.on("ready", function() {
-  doPoll();
-  lookForNewPollOrder();
-  // sendPollResults();
-
-  processTweet(tweet_team1);
-  // processTweet(tweet_team2);
-
-  //setInterval(function(){
-    //processTweet(tweet_team1);
-  //},3000);
-});
